c06c333beb10e5ab598de63edd2d58d60bcb133d
chenhaiqiang <1208105440@qq.com>
2018-07-20 14:26:40 +0800

    权限验证的添加

--- src/main/java/com/haiqiang/shiro/controller/LoginController.java
+++ src/main/java/com/haiqiang/shiro/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.haiqiang.shiro.controller;
 
 import com.haiqiang.shiro.service.UserInfoServive;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +19,47 @@ public class LoginController {
     @Autowired
     private UserInfoServive userInfoServive;
 
+    /**
+     * 主页面
+     * @return
+     */
     @RequestMapping({"","/index"})
     public String index(){
         return"/index";
     }
 
+    /**
+     * 登录页面
+     * @param map
+     * @return
+     */
     @RequestMapping("/login")
     public String login(Map<String,Object> map){
 
         map.put("nothing","没有该账户");
         return "login";
     }
+
+    /**
+     * 由访问权限的页面
+     * @return
+     */
+    @RequestMapping("/userView")
+    @RequiresPermissions("userInfo:view")
+    public String View(){
+        return "view";
+    }
+
+    /**
+     * 有删除权限的页面
+     * @return
+     */
+    @RequestMapping("/userDel")
+    @RequiresPermissions("userInfo:del")
+    public String del(){
+        return "Del";
+    }
+
     @RequestMapping("/403")
     public String unauthorizedUrl(){
         return "403";
